31321e20148913255e01a43e17d64af9769dff6b
Camiel Verdult <cpg@verdult.com>
2026-04-05 01:15:07 +0200

    get_price_at_time

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 extern crate chrono;
 
-use chrono::{DateTime, Local};
+use chrono::{DateTime, Local, Timelike};
 use reqwest;
 use serde::Deserialize;
 use tokio;
@@ -23,18 +23,18 @@ enum Leverancier {
     Generic = 0,
     All_in_power = 4,
     ANWB_Energie = 3,
-    Budget_Energie = 15,
-    Coolblue_Energie = 10,
-    Delta_Energie = 22,
+    BudgetEnergie = 15,
+    CoolblueEnergie = 10,
+    DeltaEnergie = 22,
     easyEnergy = 5,
     Eneco = 17,
-    Energie_VanOns = 6,
+    EnergieVanOns = 6,
     Energiedirect = 16,
     Energiek = 21,
     EnergyZero = 7,
     Engie = 23,
     Essent = 20,
-    Frank_Energie = 8,
+    FrankEnergie = 8,
     GroeneStroomLokaal = 9,
     NextEnergy = 11,
     Oxxio = 19,
@@ -70,12 +70,35 @@ async fn get_data(
     })
 }
 
+fn get_price_at_time(prices: &PricingDataResponse, time: DateTime<Local>) -> Option<f64> {
+    let hour = time.hour(); // 0-23
+    let minute = time.minute(); // 0-59
+
+    // Integer division by 15 gives us 0, 1, 2, or 3!
+    let quarter = minute / 15;
+
+    let index = (hour * 4 + quarter) as usize;
+
+    assert!(prices.purchase_price.len() >= index);
+    assert!(prices.taxes.len() >= index);
+
+    let inkoop_prijs = prices.purchase_price.get(index).copied()?;
+
+    let inkoop_vergoeding = prices.purchasing_fee;
+    let taxes = prices.taxes.get(index).copied()?;
+
+    Some(inkoop_prijs + inkoop_vergoeding + taxes)
+}
+
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let client = reqwest::Client::new();
 
     let data = get_data(client, Leverancier::Zonneplan).await?;
 
-    println!("{:#?}", data);
+    let price_now = get_price_at_time(&data.pricings, Local::now());
+
+    println!("{}", price_now.unwrap());
+
     Ok(())
 }
